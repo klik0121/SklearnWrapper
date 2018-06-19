f2a2297a426d5b8194685339bcfb7ad162155b10
klik0121 <dru0121@gmail.com>
2018-06-19 12:51:18 +0300

    file data source

--- SklearnWrapper/Datasets.py
+++ SklearnWrapper/Datasets.py
@@ -16,10 +16,11 @@ from os import makedirs
 def get_dataset_dict():
     """Получить список методов. Индекс 0 - имя метода, индекс 1 - тело метода"""
     method_list = getmembers(sklearn.datasets, isfunction) + getmembers(sklearn.datasets.samples_generator)
-    method_list.append(["make_download", make_download])
+    method_list.append(["get_from_url", get_from_url])
+    method_list.append(["get_from_file", get_from_file])
 
     return {method[0]:method[1] for method in method_list
-            if method[0].startswith('make_')}
+            if method[0].startswith('make_') or method[0].startswith('get_from_')}
 
 def get_gen_dict():
     """Получить сигнатуру методов."""
@@ -27,7 +28,7 @@ def get_gen_dict():
             for k, v in get_dataset_dict().items()}
 
 
-def make_download(url:str, dataset_name:str='toy', delimiter:str='\t', cache:bool=True):
+def get_from_url(url:str, dataset_name:str='toy', delimiter:str='\t', cache:bool=True):
     """Загружает файл, кэширует при надобности"""
     dir = dirname(abspath(__file__)) + '\\datasets'
     if not exists(dir):
@@ -36,12 +37,19 @@ def make_download(url:str, dataset_name:str='toy', delimiter:str='\t', cache:boo
     if cache:
         if not exists(file_name):
             download(url, file_name)
-        arr = np.genfromtxt(file_name, dtype=float, delimiter = delimiter)
+        return get_from_file(file_name, delimiter)
     else:
         arr = np.genfromtxt(url, delimiter = delimiter)
+        return get_from_array(arr)
+
+def get_from_array(arr):
     sh = np.shape(arr)
     cols_num = sh[1]
     return arr[:, 0: cols_num - 2], arr[:, cols_num - 1]
+
+def get_from_file(file_name:str, delimiter:str):
+    arr = np.genfromtxt(file_name, dtype=float, delimiter = delimiter)
+    return get_from_array(arr)
 
 def download(url:str, file_name:str):
     """Загружает файл по заданному URL"""
